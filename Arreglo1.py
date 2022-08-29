c6865c098e64cfa06524f9a9307b4b4030528b97
lalala <maria.martinez14140@est.uca.edu.nigit config --global user.email maria.martinez14140@est.uca.edu.ni>
2022-08-29 09:46:38 -0600

    Ejercicio 1 de arreglos

---
+++ Arreglo1.py
@@ -0,0 +1,11 @@
+numeros = []
+numeros.append(3)
+numeros.append(8)
+numeros.append(9)
+numeros.append(20)
+print("Numeros de arreglos")
+print(numeros)
+numeros.insert(1, 15)
+print(numeros)
+numeros.remove(20)
+print(numeros)
